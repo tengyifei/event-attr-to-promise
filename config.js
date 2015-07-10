381194f3e14741a32ca0259c3e63bf809020fdde
Yifei Teng <yifei@skycatch.com>
2015-07-09 17:55:00 -0700

    Use bluebird

--- config.js
+++ config.js
@@ -1,15 +1,23 @@
 System.config({
+  "defaultJSExtensions": true,
   "transpiler": "traceur",
   "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   }
 });
 
 System.config({
   "map": {
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+    "bluebird": "npm:bluebird@2.9.33",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.90",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:bluebird@2.9.33": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
   }
 });
 
